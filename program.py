91182b7b6811883bf53eb5724b5de1ccf7ae810d
George Wortmann <gf.wortmann@gmail.com>
2023-03-29 23:31:05 +0300

    task 6 committed 1

--- program.py
+++ program.py
@@ -1,23 +1,37 @@
-# Задача 4: Петя, Катя и Сережа делают из бумаги журавликов. Вместе
-# они сделали S журавликов. Сколько журавликов сделал каждый
-# ребенок, если известно, что Петя и Сережа сделали одинаковое
-# количество журавликов, а Катя сделала в два раза больше журавликов,
-# чем Петя и Сережа вместе?
+#Задача 6: Вы пользуетесь общественным транспортом?
+#Вероятно, вы расплачивались за проезд и получали билет с номером.
+#Счастливым билетом называют такой билет с шестизначным номером,
+#где сумма первых трех цифр равна сумме последних трех. 
+#Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. 
+#Вам требуется написать программу, которая проверяет счастливость билета.
+
+
 from curses.ascii import isdigit
 import math
 
+def half (entered, head:bool):
+    res = 0
+    if head:
+        countRange = [0,3]
+    else:
+        countRange = [3,6]
+
+    for i in entered[ countRange[0] : countRange[1] ]:        
+        res+=int(i)
+    return res
+
 
 while True:
-    s = input('Enter how many paper cranes have made all the kids? (Number must be natural) :')
-    try:
-        s = int(s)
-        if s > 0:
-            if s % 6 == 0:
-                print (f'Katya has made {s//3*2} paper cranes and every boy by {s//6} ones')
-                break
-            else: 
-                print ('Sorry, number of paper cranes must be a multiple of six')
-        else: 
-            print(f'Sorry, {s} isn\'t a natural number')
-    except :
-        print(f'Sorry, you\'ve entered incorrect number or not a number..')
+    ticket_id = input ("enter the ticket id: ")
+    if len(ticket_id) != 6:
+        print("count of digits must be equal to six!")
+    else:
+        if ticket_id.isdigit():
+            break
+    print("ticket id must contain only digits!")
+
+if half (ticket_id, True) == half (ticket_id, False):
+    print('Your ticket is happy')
+else:
+    print('Your ticket is NOT happy, sorry... Try another.')
+
